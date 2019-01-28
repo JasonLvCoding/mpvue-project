d1281a7a028d3972375a6f600cab0f62999f6393
zhangsong <zhangsong@beeboxes.com>
2019-01-28 11:33:46 +0800

    add tabBar

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -160,48 +160,6 @@ export default {
     }
   },
 
-  // 这个字段走 app.json
-  config: {
-    pages: [], // Will be filled in webpack
-    window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'weui for mpvue',
-      navigationBarTextStyle: 'black'
-    },
-    tabBar: {
-      color: '#999999',
-      selectedColor: '#1AAD16',
-      backgroundColor: '#ffffff',
-      borderStyle: 'white',
-      list: [
-        {
-          pagePath: 'pages/tabbar/tabbar',
-          text: '微信',
-          iconPath: 'static/images/icon_nav_button.png',
-          selectedIconPath: 'static/images/icon_nav_button.png'
-        },
-        {
-          pagePath: 'pages/tabbar/tabbar',
-          text: '通讯录',
-          iconPath: 'static/images/icon_nav_cell.png',
-          selectedIconPath: 'static/images/icon_nav_cell.png'
-        },
-        {
-          pagePath: 'pages/tabbar/tabbar',
-          text: '发现',
-          iconPath: 'static/images/icon_nav_cell.png',
-          selectedIconPath: 'static/images/icon_nav_cell.png'
-        },
-        {
-          pagePath: 'pages/tabbar/tabbar',
-          text: '我',
-          iconPath: 'static/images/icon_nav_toast.png',
-          selectedIconPath: 'static/images/icon_nav_toast.png'
-        }
-      ]
-    }
-  }
 }
 </script>
 <style>
